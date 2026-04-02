1c8fd505d68daf463df22f566dd4b86f456871e6
Ahmed AlMoselhy <ahmedalmoselhy.slm@gmail.com>
2026-04-02 12:32:51 +0200

    Update project technologies list to include additional frameworks and tools

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -112,7 +112,7 @@ export const projectsData: Project[] = [
       "University management system for handling academic operations.",
     extendedDescription:
       "This is a personal and very dear project to my heart, I've had it in my backlog for a log time, but now I'm officially starting my work on it, this project will have so much details and many many technoligical challanges, which I'm so happy and excited to tackle, and to make matters more exciting, I'll work on this project multiple times wilth more than one framework/technology. I might not be able to finalize every single one of these clones, but I'm excited to tackle and work with something new to me. This repository is just my personal documentations for the project, with links to all other repositories with that house the project with each technology separate, will update this repository with some stuff like database schema, phases of development of each clone, design and APIs documentation.",
-    technologies: ["PHP", "Laravel", "MySQL", "JavaScript"],
+    technologies: ["PHP", "Laravel", "MySQL", "JavaScript", "Laravel Blade", "Markdown", "Node.js", "Express.js", "Python", "Django", "React", "TypeScript", "Tailwind CSS"],
     image: "/projects-images/unione.png",
     githubUrl: "https://github.com/ahmedmalmoselhy/UniOne",
     liveUrl: "",
